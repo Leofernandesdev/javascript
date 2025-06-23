89193637f1ab179d5467a6e3d36e76c473cbd126
leonardo fernandes de oliveira <leoolivera.pro@gmail.com>
2025-06-23 13:46:33 -0300

    gemini

---
+++ Cursojs/gemini/script.js
@@ -0,0 +1,15 @@
+
+function mostrarSaudacao() {
+  console.log("Olá! Bem-vindo ao mundo das funções!");
+}
+
+function saudarPessoa(nome) {
+  console.log("Olá, " + nome + "! Como vai você?");
+}
+
+console.log("O script foi carregado! A chamar as funções agora...");
+
+
+saudarPessoa("Maria");
+saudarPessoa("João");
+saudarPessoa("Alex");
